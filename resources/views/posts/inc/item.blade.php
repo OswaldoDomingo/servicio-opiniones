0be74d3fd03356128fa927590420248baa3bbb47
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-01-24 18:25:54 +0100

    Video Filtrado de registro

--- resources/views/posts/inc/item.blade.php
+++ resources/views/posts/inc/item.blade.php
@@ -1,5 +1,7 @@
  <!-- Si se va a Post hay un método que se llama user y con eso se trae el nombre del usuario -->
- <a href="#" class="text-lg font-semibold">{{ $post->user->name }}</a>
+ <a href="{{ route('users.show', $post->user->id) }}" class="text-lg font-semibold">
+    {{ $post->user->name }}
+</a>
  <p class="mt-1 text-xs">
      <em>
          {{ $post->created_at->format('d/m/Y') }}
